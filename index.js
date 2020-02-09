906b13ea97339052ea2bf3954703a14221daaf15
Gisele <giskey11@gmail.com>
2020-02-08 23:31:07 -0600

    pdf working

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
-// 1. set up dependencies => always bring the package to your server by doing 
-
-//const <name> = require("<name>");
 const inquirer = require("inquirer");
 const axios = require("axios");
 const generateHTML = require("./Assets/generateHTML");
-const fs = require('fs');
+var fs = require("fs");
 const convertFactory = require('electron-html-to');
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 //setup the questions
 const questions = [
-    {
-        type: "input",
-        name: "username",
-        message: "What's Your GitHub User Name? "
-    },
-    {
-        type: "list",
-        name: "favColor",
-        message: "What's You're Favorate Color ? ",
-        choices: ["green", "blue","pink","red"]
-    }
+  {
+    type: "input",
+    name: "username",
+    message: "What's Your GitHub User Name? "
+  },
+  {
+    type: "list",
+    name: "favColor",
+    message: "What's You're Favorate Color ? ",
+    choices: ["green", "blue", "pink", "red"]
+  }
 ];
 
-inquirer.prompt(questions).then(function (response) {
- 
-    // use axios to get the data from github
-    const mainUrl = "https://api.github.com/users/" + response.username;
-    const startUrl = "https://api.github.com/users/" + response.username + "/starred";
+inquirer.prompt(questions).then(function(response) {
+  console.log(response);
+  // use axios to get the data from github
+  const mainUrl = "https://api.github.com/users/" + response.username;
+  const startUrl =
+    "https://api.github.com/users/" + response.username + "/starred"; // "stargazers_count": 1,
 
-    //axios call
-    axios.get(mainUrl).then(function (response) {
-         
-        response.data.map(eles=>{
-            console.log(eles)
-        })
-          
-      //  console.log(response.data);
+  //axios call
+  axios.get(mainUrl).then(function({ data }) {
+    console.log(data);
+    // 2nd axios to get the starts count
+    axios.get(startUrl).then(function(res) {
+      const starCount = res.data.map(elt => {
+        return elt.stargazers_count;
+      });
 
-        // pdf.create(generateHTML(els)).toFile('./devportfolio.pdf', function (err, res) {
-        //     if (err) throw (err);
-        // })
+      const totalCount = starCount.length;
+      //   console.log(totalCount);
 
+      //build the parameters to populate the hmtl
+      const htmlData = {
+        color: response.favColor,
+        avatar_url: data.avatar_url,
+        name: data.name,
+        location: data.location,
+        company: data.company,
+        html_url: data.html_url,
+        blog: data.blog,
+        bio: data.bio,
+        public_repos: data.public_repos,
+        followers: data.followers,
+        stars: totalCount,
+        following: data.following
+      };
 
-        //build the html page and use electron to pdf
-        // var conversion = convertFactory({
-        //     converterPath: convertFactory.converters.PDF
-        // });
+      //generate hmtl
+      var htmlCode = generateHTML(htmlData);
+      console.log(htmlCode);
 
-        // conversion({ html: generateHTML(data) }, function (err, result) {
-        //     if (err) {
-        //         return console.error(err);
-        //     }
+      //write the page so electrron can intercept the html page... very important.
+      writeFileAsync("./index.html", htmlCode);
 
-        //     result.stream.pipe(fs.createWriteStream('./pdf/generatedPDF.pdf'));
-        //     conversion.kill(); // necessary if you use the electron-server strategy, see bellow for detai
-        // })
-    })
+      //build the pdf using electron ./pdf/generatedPDF.pdf
+      const conversion = convertFactory({
+        converterPath: convertFactory.converters.PDF
+      });
+
+      conversion({ html: htmlCode }, function(err, result) {
+        if (err) throw err;
+        result.stream.pipe(fs.createWriteStream('./profile.pdf'));
+        conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+      });
+    });
+  });
 });
